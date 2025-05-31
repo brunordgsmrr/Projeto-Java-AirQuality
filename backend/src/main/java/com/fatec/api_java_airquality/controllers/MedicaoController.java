fd19db0c4ef74b15392e06d41cef2217579ce2d7
brunordgsmrr <bruno_rdgs_@hotmail.com>
2025-05-31 17:44:47 -0300

    feat: cria a classe MedicacaoController

---
+++ backend/src/main/java/com/fatec/api_java_airquality/controllers/MedicaoController.java
@@ -0,0 +1,22 @@
+package com.fatec.api_java_airquality.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
+public class MedicaoController {
+	
+	@Autowired
+	MedicaoService medicaoService;
+	
+	@GetMapping(value="/medicao")
+	public void getMedicao() {
+		
+	}
+	
+	
+
+}
